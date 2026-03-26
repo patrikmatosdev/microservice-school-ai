115be9f6ccaa5023b3ab71313590b7ff60190712
Patrik Matos <patrikmatos.tecinfo@gmail.com>
2026-03-25 23:00:56 -0300

    intialize project

---
+++ src/test/kotlin/microservice/school/ai/AiApplicationTests.kt
@@ -0,0 +1,13 @@
+package microservice.school.ai
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
